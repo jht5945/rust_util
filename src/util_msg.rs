9c1bed76d1408467ff09a97a2783848751de8ba0
Hatter Jiang <jht5945@gmail.com>
2019-11-23 21:09:42 +0800

    add terminal check

--- src/util_msg.rs
+++ src/util_msg.rs
@@ -2,19 +2,33 @@ use std::{
     io::{self, Write},
 };
 
+lazy_static! {
+    pub static ref IS_ATTY: bool = is_atty();
+}
+
 pub enum MessageType { INFO, OK, WARN, ERROR, DEBUG, }
+
+pub fn is_atty() -> bool{
+    let isatty = unsafe { libc::isatty(libc::STDOUT_FILENO as i32) } != 0;
+    isatty
+}
 
 pub fn print_color(color: Option<term::color::Color>, is_bold: bool, m: &str) {
     let mut t = term::stdout().unwrap();
-    match color {
-        Some(c) => t.fg(c).unwrap(),
-        None => (),
-    }
-    if is_bold {
-        t.attr(term::Attr::Bold).unwrap();
-    }
-    write!(t, "{}", m).unwrap();
-    t.reset().unwrap();
+    match *IS_ATTY {
+        true => {
+            match color {
+                Some(c) => t.fg(c).unwrap(),
+                None => (),
+            }
+            if is_bold {
+                t.attr(term::Attr::Bold).unwrap();
+            }
+            write!(t, "{}", m).unwrap();
+            t.reset().unwrap();
+        },
+        false => write!(t, "{}", m).unwrap(),
+    };
 }
 
 pub fn print_color_and_flush(color: Option<term::color::Color>, is_bold: bool, m: &str) {
