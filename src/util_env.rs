34df5b66a2b8149f466386e2cc348f7279d543c1
Hatter Jiang <jht5945@gmail.com>
2020-04-12 18:19:34 +0800

    ref is_env_on

--- src/util_env.rs
+++ src/util_env.rs
@@ -2,5 +2,5 @@
 use std::env;
 
 pub fn is_env_on(var: &str) -> bool {
-    env::var(var).map(|v| v.to_lowercase()).map(|v| (v == "true" || v == "yes" || v == "1")).unwrap_or(false)
+    env::var(var).map(|v| v.to_lowercase()).map(|v| vec!["true", "yes", "1"].iter().any(|x| x == &v)).unwrap_or(false)
 }
