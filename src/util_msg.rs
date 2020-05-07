0ebd4e56304d39b7026fe9697a3a2c89a777b26c
Hatter Jiang <jht5945@gmail.com>
2020-05-07 08:21:19 +0800

    fix print_color unwarp issue

--- src/util_msg.rs
+++ src/util_msg.rs
@@ -17,18 +17,22 @@ pub fn is_atty() -> bool {
 }
 
 pub fn print_color(color: Option<term::color::Color>, is_bold: bool, m: &str) {
-    let mut t = term::stdout().unwrap();
-    if *IS_ATTY {
-        if let Some(c) = color {
-            t.fg(c).unwrap();
-        }
-        if is_bold {
-            t.attr(term::Attr::Bold).unwrap();
-        }
-        write!(t, "{}", m).unwrap();
-        t.reset().unwrap();
-    } else {
-        write!(t, "{}", m).unwrap();
+    match term::stdout() {
+        Some(mut t) => {
+            if *IS_ATTY {
+                if let Some(c) = color {
+                    t.fg(c).ok();
+                }
+                if is_bold {
+                    t.attr(term::Attr::Bold).ok();
+                }
+                write!(t, "{}", m).ok();
+                t.reset().ok();
+            } else {
+                write!(t, "{}", m).ok();
+            }
+        },
+        None => print!("{}", m),
     }
 }
 
